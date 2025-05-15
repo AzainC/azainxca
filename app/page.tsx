8a72e471cb3050d5f131a1fa1eb747814ca47c3d
Azain <azaincardoz09@gmail.com>
2025-05-15 19:22:46 +0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
   return (
     <>
